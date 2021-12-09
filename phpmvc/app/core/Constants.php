91fd8ce2f658236eb992baeb4ca6963d5123ea1e
irfaudarajat <irfaudarajat@gmail.com>
2021-12-09 18:21:25 +0700

    Menyelesaikan video materi asset di pertemuan ke 4

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL','http://localhost/prakweb2021_phpmvc_193040120/phpmvc/public');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,7 @@
-
-    <h1>About me</h1>
+<div class="container">
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/xiix.jpg" alt="Irfa" width="400" class="rounded-circle shadow">
     <p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+
+    </div>
